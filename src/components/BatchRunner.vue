0cd825b88859cbf6b51eb4c1f97b3aed803474d8
Jasper van Gestel <jvangestel@gmail.com>
2022-09-29 10:09:24 +0200

    initial commit

---
+++ src/components/BatchRunner.vue
@@ -0,0 +1,147 @@
+<template>
+  <div>
+    <h3 v-if="title">{{ title }}</h3>
+    <button v-if="!finished && !emergencyStop" @click="start"
+      class="btn btn-primary" :disabled="running || !initialized">
+      <template v-if="initialized">{{ translations.start }}</template>
+      <template v-if="!initialized">Loading...</template>
+    </button>
+    <button v-if="finished || emergencyStop" @click="restart" class="btn btn-primary">
+      {{ translations.restart }}
+    </button>
+    <progress-bar :progress="progress" />
+    <button v-if="!finished" @click="abort" class="btn btn-danger" :disabled="!running">
+      {{ translations.cancel }}
+    </button>
+    <div v-if="messages.length" class="alert alert-info" role="alert">
+      <ul>
+        <li v-for="index, message in messages" :key="index">{{ message }}</li>
+      </ul>
+    </div>
+    <div v-if="info.length" class="panel panel-default">
+      <div class="panel-body">
+        <p v-for="index, message in info" :key="index" class="info">
+          {{ message }}
+        </p>
+      </div>
+    </div>
+
+  </div>
+</template>
+<script>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import ProgressBar from './ProgressBar.vue';
+
+export default {
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+    autostart: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  components: {
+    ProgressBar,
+  },
+  setup(props) {
+    const currentUrl = window.location.pathname;
+    const finished = ref(false);
+    const info = ref([]);
+    const initialized = ref(false);
+    const messages = ref([]);
+    const progress = ref(0);
+    const emergencyStop = ref(false);
+    const running = ref(false);
+    const translations = ref({
+      start: 'Start',
+      cancel: 'Cancel',
+      restart: 'Restart',
+    });
+
+    const abort = (() => {
+      emergencyStop.value = true;
+    });
+
+    const fetchProgress = (async () => {
+      const runUrl = `${currentUrl}?progress=run`;
+      console.log(currentUrl, runUrl);
+      const result = await axios.get(runUrl);
+      if ('data' in result) {
+        if ('percent' in result.data) {
+          progress.value = result.data.percent * 100;
+          if (progress.value === 100) {
+            finished.value = true;
+          }
+        }
+        if ('messages' in result.data && result.data.messages !== null) {
+          messages.value = result.data.messages;
+        }
+      }
+      console.log(result.data);
+    });
+
+    const start = (async () => {
+      running.value = true;
+      while (finished.value !== true && emergencyStop.value !== true) {
+        // eslint-disable-next-line no-await-in-loop
+        await fetchProgress();
+      }
+      running.value = false;
+    });
+
+    const init = (async () => {
+      const initUrl = `${currentUrl}?progress=init`;
+      const result = await axios.get(initUrl);
+      if ('data' in result) {
+        initialized.value = true;
+        if ('translations' in result.data) {
+          translations.value = result.data.translations;
+        }
+        if ('info' in result.data) {
+          let infoArray = result.data.info;
+          if (!Array.isArray(infoArray)) {
+            infoArray = [infoArray];
+          }
+          info.value = infoArray;
+        }
+      }
+    });
+
+    const restart = (async () => {
+      const resetUrl = `${currentUrl}?progress=restart`;
+      const result = await axios.get(resetUrl);
+      if ('data' in result) {
+        progress.value = 0;
+        finished.value = false;
+        messages.value = [];
+        emergencyStop.value = false;
+      }
+    });
+
+    onMounted(async () => {
+      await init();
+      if (props.autostart) {
+        start();
+      }
+    });
+
+    return {
+      abort,
+      emergencyStop,
+      finished,
+      info,
+      initialized,
+      messages,
+      progress,
+      restart,
+      running,
+      start,
+      translations,
+    };
+  },
+};
+</script>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="progress" style="position: relative">
+    <div class="progress-bar progress-bar-success" role="progressbar" :aria-valuenow="progress"
+      aria-valuemin="0" aria-valuemax="100" :style="progressWidth">
+    </div>
+    <div class="progress-text">
+      {{ progress.toFixed(2) }}%
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  name: 'ProgressBar',
+  props: {
+    progress: {
+      type: Number,
+      default: 0,
+    },
+  },
+  setup(props) {
+    const progressWidth = computed(() => `width: ${props.progress}%;`);
+
+    return {
+      progressWidth,
+    };
+  },
+};
+</script>
+<style scoped>
+  .progress {
+    position: relative;
+  }
+  .progress-text {
+    color: #333;
+    text-align: center;
+    width: 100%;
+    position: absolute;
+  }
+</style>
