89ba1f8db900a444b2e6b4c66bdda8d647c8ce4c
Jasper van Gestel <jvangestel@gmail.com>
2022-10-06 21:10:27 +0200

    Add pinia

--- src/gems-vue.js
+++ src/gems-vue.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue';
-import BatchRunner from './components/BatchRunner.vue';
+import { createPinia } from 'pinia';
+import BatchRunner from './vue/components/BatchRunner.vue';
+import ShowRespondent from './vue/components/ShowRespondent.vue';
 
 const appSettings = {
   components: {
     BatchRunner,
+    ShowRespondent,
   },
 };
 
 const app = createApp(appSettings);
+const pinia = createPinia();
+app.use(pinia);
 app.mount('#app');
 export default app;
