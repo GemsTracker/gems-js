95eae3042c10e5f537ec04068f72573310d96f72
Jasper van Gestel <jvangestel@gmail.com>
2022-11-11 14:10:54 +0100

    eslint fix auth-idle-checker

--- src/authenticated/auth-idle-checker.js
+++ src/authenticated/auth-idle-checker.js
@@ -1,91 +1,95 @@
-import {url} from "../general/functions";
+import { url } from '../general/functions';
 
 export default class AuthIdleChecker {
-    constructor() {
-        this.requestTime = new Date();
-        const body = document.getElementsByTagName('body')[0];
+  constructor() {
+    this.requestTime = new Date();
+    const body = document.getElementsByTagName('body')[0];
 
-        this.maxIdleTime = parseInt(body?.getAttribute('data-max-idle-time')) || 1200;
-        this.authPollInterval = parseInt(body?.getAttribute('data-auth-poll-interval')) || 60;
+    this.maxIdleTime = parseInt(body?.getAttribute('data-max-idle-time'), 10) || 1200;
+    this.authPollInterval = parseInt(body?.getAttribute('data-auth-poll-interval'), 10) || 60;
 
-        this.init();
+    this.init();
 
-        this.schedule();
-    }
+    this.schedule();
+  }
 
-    init() {
-        const buttonAlive = document.getElementById('authIdleCheckerWarningAlive');
-        buttonAlive?.addEventListener('click', () => {
-            fetch(url('/auth/idle-alive'), {method: 'post'})
-                .then((response) => {
-                    if (!response.ok) {
-                        throw new Error();
-                    }
+  init() {
+    const buttonAlive = document.getElementById('authIdleCheckerWarningAlive');
+    buttonAlive?.addEventListener('click', () => {
+      fetch(url('/auth/idle-alive'), { method: 'post' })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error();
+          }
 
-                    return response.json();
-                })
-                .then((data) => {
-                    this.requestTime = new Date();
+          return response.json();
+        })
+        .then((data) => {
+          this.requestTime = new Date();
 
-                    this.toggleIdleLogoutWarning(false);
+          this.toggleIdleLogoutWarning(false);
 
-                    if (data.redirect) {
-                        window.location.href = data.redirect;
-                    }
-                });
+          if (data.redirect) {
+            window.location.href = data.redirect;
+          }
         });
+    });
 
-        const buttonIgnore = document.getElementById('authIdleCheckerWarningIgnore');
-        buttonIgnore?.addEventListener('click', () => {
-            this.toggleIdleLogoutWarning(false);
-        });
-    };
+    const buttonIgnore = document.getElementById(
+      'authIdleCheckerWarningIgnore',
+    );
+    buttonIgnore?.addEventListener('click', () => {
+      this.toggleIdleLogoutWarning(false);
+    });
+  }
 
-    schedule() {
-        setTimeout(() => {
-            this.check();
-            this.schedule();
-        }, this.authPollInterval * 1000);
-    };
+  schedule() {
+    setTimeout(() => {
+      this.check();
+      this.schedule();
+    }, this.authPollInterval * 1000);
+  }
 
-    check() {
-        fetch(url('/auth/idle-poll'))
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error();
-                }
-
-                return response.json();
-            })
-            .then((data) => {
-                if (data.show_idle_logout_warning) {
-                    this.toggleIdleLogoutWarning(true);
-                }
-
-                if (data.redirect) {
-                    window.location.href = data.redirect;
-                }
-            })
-            .catch(() => {
-                // Fallback in case of e.g. network failure
-                if ((+ new Date()) - (+this.requestTime) > this.maxIdleTime * 1000) {
-                    window.location.href = url('/');
-                }
-            });
-    };
-
-    toggleIdleLogoutWarning(show) {
-        const warning = document.getElementById('authIdleCheckerWarning');
-        if (warning) {
-            if (show) {
-                warning.classList.remove('hidden');
-            } else {
-                warning.classList.add('hidden');
-            }
+  check() {
+    fetch(url('/auth/idle-poll'))
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error();
         }
-    };
+
+        return response.json();
+      })
+      .then((data) => {
+        if (data.show_idle_logout_warning) {
+          this.toggleIdleLogoutWarning(true);
+        }
+
+        if (data.redirect) {
+          window.location.href = data.redirect;
+        }
+      })
+      .catch(() => {
+        // Fallback in case of e.g. network failure
+        if (+new Date() - +this.requestTime > this.maxIdleTime * 1000) {
+          window.location.href = url('/');
+        }
+      });
+  }
+
+  toggleIdleLogoutWarning(show) {
+    const warning = document.getElementById('authIdleCheckerWarning');
+    if (warning) {
+      if (show) {
+        warning.removeAttribute('hidden');
+        warning.classList.remove('hidden');
+      } else {
+        warning.setAttribute('hidden', '');
+        warning.classList.add('hidden');
+      }
+    }
+  }
 }
 
 window.addEventListener('load', () => {
-    const authIdleChecker = new AuthIdleChecker();
+  new AuthIdleChecker();
 });
