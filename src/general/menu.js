f2917a07dff21076ee6430cd92e18042a0aea5e9
Jasper van Gestel <jvangestel@gmail.com>
2023-02-07 16:48:25 +0100

    add js for container-link-items

---
+++ src/general/menu.js
@@ -0,0 +1,39 @@
+export default class Menu {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    const menuContainers = document.querySelectorAll('#menu .container-link-item');
+
+    menuContainers.forEach((container) => {
+      container.addEventListener('click', (event) => {
+        if (event.target.parentNode !== container) {
+          return;
+        }
+        event.preventDefault();
+        let submenuList = event.target.nextElementSibling;
+        console.log(submenuList);
+        while (submenuList && !submenuList.classList.contains('menu-children')) {
+          submenuList = submenuList.nextElementSibling;
+        }
+        if (submenuList) {
+          if (submenuList.classList.contains('closed')) {
+            submenuList.classList.add('open');
+            submenuList.classList.remove('closed');
+            container.classList.add('open');
+            container.classList.remove('closed');
+          } else {
+            submenuList.classList.add('closed');
+            submenuList.classList.remove('open');
+            container.classList.add('closed');
+            container.classList.remove('open');
+          }
+        }
+      });
+    });
+  }
+}
+window.addEventListener('load', () => {
+  const menu = new Menu();
+});
