8604b5963ee98f455be7896937baa5752934e330
Jasper van Gestel <jvangestel@gmail.com>
2023-03-30 21:02:34 +0200

    add size to textelement

--- src/vue/components/GemsForm/Element/TextElement.vue
+++ src/vue/components/GemsForm/Element/TextElement.vue
@@ -3,13 +3,14 @@
     <gems-form-label :elementId="elementId" :options="options" :for="elementId" />
     <div class="element-container">
       <input v-model="formValue" type="text" :id="elementId"
-        :name="elementId" class="form-control" :disabled="disabled" />
+        :name="elementId" class="form-control" :disabled="disabled" :size="size"/>
       <gems-form-validator-messages :validator="validator" :serverValidator="serverValidator" />
       <p v-if="'description' in options" class="help-block"> {{options.description}}</p>
     </div>
   </div>
 </template>
 <script>
+import { computed } from 'vue';
 import GemsFormLabel from '../Label.vue';
 import GemsFormValidatorMessages from '../ValidatorMessages.vue';
 
@@ -36,11 +37,19 @@ export default {
       validatorClass,
     } = useGemsFormElementFunctions(props.options);
 
+    const size = computed(() => {
+      if ('size' in props.options && props.options.size) {
+        return props.options.size;
+      }
+      return null;
+    });
+
     return {
       disabled,
       elementId,
       formValue,
       serverValidator,
+      size,
       validator,
       validatorClass,
     };
