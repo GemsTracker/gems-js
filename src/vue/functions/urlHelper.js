62abcdbf3e4502ff5f6938e39d0f88702c5717eb
Jasper van Gestel <jvangestel@gmail.com>
2023-09-14 13:14:43 +0200

    Re-use checking for supplied or stored patient info

--- src/vue/functions/urlHelper.js
+++ src/vue/functions/urlHelper.js
@@ -5,6 +5,22 @@ const useUrlHelper = (() => {
   const baseStore = useBaseStore();
   const patientStore = usePatientStore();
 
+  const checkPatientData = ((patientNr, organizationId) => {
+    let checkedPatientNr = patientNr;
+    if (checkedPatientNr === null) {
+      checkedPatientNr = patientStore.patientNr;
+    }
+    let checkedOrganizationId = organizationId;
+    if (checkedOrganizationId === null) {
+      checkedOrganizationId = patientStore.organizationId;
+    }
+
+    return {
+      checkedPatientNr,
+      checkedOrganizationId,
+    };
+  });
+
   const getCarePlanDeleteUrl = ((carePlanId) => `${baseStore.baseUrl}respondent/${patientStore.patientNr}/${patientStore.organizationId}/tracks/delete/${carePlanId}`);
   const getCarePlanUnDeleteUrl = ((carePlanId) => `${baseStore.baseUrl}respondent/${patientStore.patientNr}/${patientStore.organizationId}/tracks/undelete/${carePlanId}`);
   const getCarePlanEditUrl = ((carePlanId) => `${baseStore.baseUrl}respondent/${patientStore.patientNr}/${patientStore.organizationId}/tracks/edit/${carePlanId}`);
@@ -13,27 +29,22 @@ const useUrlHelper = (() => {
 
   const getTokenAnswerUrl = ((tokenId, carePlanId) => `${baseStore.baseUrl}respondent/${patientStore.patientNr}/${patientStore.organizationId}/track/${carePlanId}/token/answer/${tokenId}`);
   const getTokenAskUrl = ((tokenId) => `${baseStore.baseUrl}ask/to-survey/${tokenId}`);
-  const getTokenShowUrl = ((tokenId, carePlanId) => `${baseStore.baseUrl}respondent/${patientStore.patientNr}/${patientStore.organizationId}/track/${carePlanId}/token/${tokenId}`);
+  const getTokenShowUrl = ((tokenId, carePlanId, patientNr = null, organizationId = null) => {
+    const { checkedPatientNr, checkedOrganizationId } = checkPatientData(patientNr, organizationId);
+    return `${baseStore.baseUrl}respondent/${checkedPatientNr}/${checkedOrganizationId}/track/${carePlanId}/token/${tokenId}`;
+  });
 
   const getTrackCreateUrl = ((trackId) => `${baseStore.baseUrl}respondent/${patientStore.patientNr}/${patientStore.organizationId}/tracks/create/${trackId}`);
 
   const getRespondentShowUrl = ((patientNr = null, organizationId = null) => {
-    let newPatientNr = patientNr;
-    if (newPatientNr === null) {
-      newPatientNr = patientStore.patientNr;
-    }
-    let newOrganizationId = organizationId;
-    if (newOrganizationId === null) {
-      newOrganizationId = patientStore.organizationId;
-    }
-    return `${baseStore.baseUrl}respondent/${newPatientNr}/${newOrganizationId}`;
+    const { checkedPatientNr, checkedOrganizationId } = checkPatientData(patientNr, organizationId);
+    return `${baseStore.baseUrl}respondent/${checkedPatientNr}/${checkedOrganizationId}`;
   });
   const getRespondentDeleteUrl = (() => `${baseStore.baseUrl}respondent/delete/${patientStore.patientNr}/${patientStore.organizationId}`);
   const getRespondentUndeleteUrl = (() => `${baseStore.baseUrl}respondent/undelete/${patientStore.patientNr}/${patientStore.organizationId}`);
 
   return {
     getCarePlanDeleteUrl,
-
     getCarePlanEditUrl,
     getCarePlanUnDeleteUrl,
     getInsertSurveyUrl,
