7404aeb04d2e1d87217f4964acda5e4473338724
Jasper van Gestel <jvangestel@gmail.com>
2023-09-14 14:02:10 +0200

    Add ExhibitorElement

---
+++ src/vue/components/GemsForm/Element/ExhibitorElement.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="form-group">
+    <gems-form-label :elementId="elementId" :options="options" />
+    <div class="value">{{formValue}}</div>
+      <input type="hidden" :name="elementId" :value="formValue" />
+  </div>
+</template>
+<script>
+import useGemsFormElementFunctions from '../../../functions/gemsFormElementFunctions';
+
+import GemsFormLabel from '../Label.vue';
+
+export default {
+  props: {
+    options: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+  },
+  components: {
+    GemsFormLabel,
+  },
+  setup(props) {
+    const { elementId, formValue } = useGemsFormElementFunctions(props.options);
+
+    return {
+      elementId,
+      formValue,
+    };
+  },
+};
+</script>
