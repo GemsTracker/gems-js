8eb3bf92b7a62c6a1e0a1824922c6158e8484b13
Matijs de Jong <mjong@magnafacta.nl>
2023-10-10 14:52:41 +0200

    Javascript functions that use to be in attributes

--- src/authenticated.js
+++ src/authenticated.js
@@ -1,6 +1,7 @@
 import './authenticated/auth-idle-checker';
 import './authenticated/autofocus';
 import './authenticated/autosubmit';
+import './authenticated/on-behave';
 import './authenticated/on-off-edit';
 import './authenticated/toggle-button';
 
