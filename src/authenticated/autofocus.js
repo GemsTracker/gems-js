9ee7ce51d1a19e1ae126ce23be07d7db97c13589
Jasper van Gestel <jvangestel@gmail.com>
2023-10-20 00:01:09 +0200

    Fix error when no form input is on page

--- src/authenticated/autofocus.js
+++ src/authenticated/autofocus.js
@@ -1,51 +1,56 @@
 export default class AutoFocus {
-    constructor() {
-        this.init();
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    const focusTracker = document.querySelector('input#auto_form_focus_tracker');
+    let currentElement;
+
+    if (focusTracker) {
+      currentElement = focusTracker.getAttribute('value');
+      // console.log(currentElement, focusTracker.getAttribute('name'));
     }
 
-    init() {
-        var focusTracker = document.querySelector('input#auto_form_focus_tracker');
-        var currentElement;
+    if (!currentElement) {
+      const firstInput = document.querySelector('form input');
+      if (firstInput) {
+        currentElement = firstInput.getAttribute('id');
+      }
+    }
+    if (currentElement) {
+      const current = document.querySelector(`#${currentElement}`);
 
-        if (focusTracker) {
-            currentElement = focusTracker.getAttribute('value');
-            // console.log(currentElement, focusTracker.getAttribute('name'));
+      current.focus();
+      if (current.scrollIntoView) {
+        current.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      }
+      try {
+        if (current.select) {
+          current.select();
         }
-
-        if (! currentElement) {
-            currentElement = document.querySelector('form input').getAttribute('id');
-        }
-        if (currentElement) {
-            var current = document.querySelector('#' + currentElement);
-
-            current.focus();
-            if (current.scrollIntoView) {
-                current.scrollIntoView({behavior: 'smooth', block: 'center'});
-            }
-            try {
-                if (current.select) {
-                    current.select();
-                }
-            } catch (ex) {}
-        }
-
-        if (focusTracker) {
-            document.querySelectorAll('form input, form select, form textarea').forEach((inputElement) => {
-                if ('submit' !== inputElement.getAttribute('type')) {
-                    this.addTracker(inputElement, focusTracker);
-                }
-            });
-        }
+      } catch (ex) {
+        // do nothing
+      }
     }
 
-    addTracker(currentElement, focusTracker) {
-        currentElement.addEventListener('focus', function () {
-            // console.log(currentElement.getAttribute('id'));
-            focusTracker.setAttribute('value', currentElement.getAttribute('id'));
-        });
+    if (focusTracker) {
+      document.querySelectorAll('form input, form select, form textarea').forEach((inputElement) => {
+        if (inputElement.getAttribute('type') !== 'submit') {
+          this.addTracker(inputElement, focusTracker);
+        }
+      });
     }
+  }
+
+  addTracker(currentElement, focusTracker) {
+    currentElement.addEventListener('focus', () => {
+      // console.log(currentElement.getAttribute('id'));
+      focusTracker.setAttribute('value', currentElement.getAttribute('id'));
+    });
+  }
 }
 
 window.addEventListener('load', () => {
-    const autoFocus = new AutoFocus();
+  const autoFocus = new AutoFocus();
 });
