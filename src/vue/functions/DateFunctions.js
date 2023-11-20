9a24b4fc11889712dc22543e3883f0220d0a5628
Jasper van Gestel <jvangestel@gmail.com>
2023-11-20 14:38:52 +0100

    Fix datefunctions to use date-fns for correct format date

--- src/vue/functions/DateFunctions.js
+++ src/vue/functions/DateFunctions.js
@@ -1,17 +1,28 @@
 import { useI18n } from 'vue-i18n';
+import { format } from 'date-fns';
+import { enGB, nl } from 'date-fns/locale';
 
 const useDateFunctions = (() => {
   const { locale } = useI18n();
 
-  const formatJsonDateTime = ((dateString, options = null) => {
-    const dateObject = new Date(dateString);
-    return dateObject.toLocaleString(locale.value, options);
+  const getDateFnsLocale = (() => {
+    if (locale.value === 'en') {
+      return enGB;
+    }
+    if (locale.value === 'nl') {
+      return nl;
+    }
+    return enGB;
   });
-  const formatJsonDate = ((dateString) => formatJsonDateTime(dateString, {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  }));
+
+  const formatJsonDateTime = ((dateString, dateFormat = 'dd-MM-yyyy HH:mm:ss') => {
+    if (dateString === null) {
+      return null;
+    }
+    const dateObject = new Date(dateString);
+    return format(dateObject, dateFormat, getDateFnsLocale());
+  });
+  const formatJsonDate = ((dateString) => formatJsonDateTime(dateString, 'dd-MM-yyyy'));
 
   return {
     formatJsonDate,
