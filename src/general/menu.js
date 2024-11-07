c1b38ac6f74ee5c45ff725478745db5c1b776d2a
Jasper van Gestel <jvangestel@gmail.com>
2024-11-07 17:25:16 +0100

    re-add clickable menu

--- src/general/menu.js
+++ src/general/menu.js
@@ -5,15 +5,46 @@ export default class Menu {
   #showMenu = false;
 
   constructor() {
-    window.addEventListener('load', () => {
-      this.#button = document.getElementById('menu-toggle');
-      this.#menu = document.getElementById('menu');
-      this.#backdrop = document.getElementById('backdrop');
+    this.init();
 
-      if (this.#menu !== null && this.#button !== null && this.#backdrop !== null) {
-        this.#button.addEventListener('click', () => this.#toggle());
-        this.#backdrop.addEventListener('click', () => this.#hide());
-      }
+    this.#button = document.getElementById('menu-toggle');
+    this.#menu = document.getElementById('menu');
+    this.#backdrop = document.getElementById('backdrop');
+
+    if (this.#menu !== null && this.#button !== null && this.#backdrop !== null) {
+      this.#button.addEventListener('click', () => this.#toggle());
+      this.#backdrop.addEventListener('click', () => this.#hide());
+    }
+  }
+
+  init() {
+    const menuContainers = document.querySelectorAll('#menu .container-link-item');
+
+    menuContainers.forEach((container) => {
+      container.addEventListener('click', (event) => {
+        if (event.target.parentNode !== container) {
+          return;
+        }
+        event.preventDefault();
+        let submenuList = event.target.nextElementSibling;
+        console.log(submenuList);
+        while (submenuList && !submenuList.classList.contains('menu-children')) {
+          submenuList = submenuList.nextElementSibling;
+        }
+        if (submenuList) {
+          if (submenuList.classList.contains('closed')) {
+            submenuList.classList.add('open');
+            submenuList.classList.remove('closed');
+            container.classList.add('open');
+            container.classList.remove('closed');
+          } else {
+            submenuList.classList.add('closed');
+            submenuList.classList.remove('open');
+            container.classList.add('closed');
+            container.classList.remove('open');
+          }
+        }
+      });
     });
   }
 
@@ -36,6 +67,7 @@ export default class Menu {
     this.#menu.classList.remove('show-menu');
     this.#backdrop.classList.remove('show');
   }
-};
-
-new Menu();
+}
+window.addEventListener('load', () => {
+  const menu = new Menu();
+});
