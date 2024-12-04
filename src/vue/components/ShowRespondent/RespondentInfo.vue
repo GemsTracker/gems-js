f53e2c9b171b05833bcc4e85fe9c1da08f6637a3
Jasper van Gestel <jvangestel@gmail.com>
2024-12-04 22:18:26 +0100

    update respondent info

--- src/vue/components/ShowRespondent/RespondentInfo.vue
+++ src/vue/components/ShowRespondent/RespondentInfo.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="patient-info">
     <loading-screen v-if="loading === true" size="3rem" />
-    <dt>Patient nr</dt><dd>{{ patientNr }}&nbsp;</dd>
-    <dt>Patient</dt><dd>{{ fullName }}&nbsp;</dd>
-    <dt>Geboortedatum</dt><dd>{{ birthdayDmy }}&nbsp;</dd>
-    <dt>Email</dt><dd><a :href="`mailto:${email}`">{{ email }}</a>&nbsp;</dd>
-    <dt>Phonenumber</dt><dd>{{ phoneNumber }}&nbsp;</dd>
+    <div class="info-block">
+      <dt>Patient nr</dt><dd>{{ patientNr }}&nbsp;</dd>
+    </div>
+    <div class="info-block">
+      <dt>Patient</dt><dd>{{ fullName }}&nbsp;</dd>
+    </div>
+    <div class="info-block">
+      <dt>Geboortedatum</dt><dd>{{ birthdayDmy }}&nbsp;</dd>
+    </div>
+    <div class="info-block">
+      <dt>Email</dt><dd><a :href="`mailto:${email}`">{{ email }}</a>&nbsp;</dd>
+    </div>
+    <div class="info-block">
+      <dt>Phonenumber</dt><dd>{{ phoneNumber }}&nbsp;</dd>
+    </div>
   </div>
 </template>
 <script>
@@ -47,6 +57,21 @@ export default {
 <style lang="scss" scoped>
 .patient-info {
   position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  margin: .5rem;
+
+  .info-block {
+    flex: 50%;
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    border-bottom: 1px solid #ddd;
+    dd, dt {
+      flex: 50%;
+    }
+  }
+
   .loader-container {
     left: 5rem;
     position: absolute;
