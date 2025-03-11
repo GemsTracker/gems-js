e8b94908a9d52678ae575c4136d0554a6c039b87
Jasper van Gestel <jvangestel@gmail.com>
2025-03-11 10:47:54 +0100

    Add care-plan organization to url, in case that care plan is from a different organization

--- src/vue/components/ShowRespondent/CareplanTableBlock.vue
+++ src/vue/components/ShowRespondent/CareplanTableBlock.vue
@@ -16,7 +16,7 @@
         <tr v-for="(carePlan, index) in filteredCarePlans" :key="index"
             :class="{deleted: isDeleted(carePlan)}">
           <td>
-              <a :href="getCarePlanShowUrl(carePlan.id)" class="btn">{{ t('Show')}}</a>
+              <a :href="getCarePlanShowUrl(carePlan.id, null, getCarePlanOrganization(carePlan))" class="btn">{{ t('Show')}}</a>
           </td>
           <td>{{ carePlan.title }}</td>
           <td>{{ carePlan.description }}</td>
@@ -32,7 +32,7 @@
           <td>{{ getCarePlanCreator(carePlan) }}</td>
           <td>
               <a v-if="carePlan.status === 'active'"
-                 :href="getCarePlanEditUrl(carePlan.id)"
+                 :href="getCarePlanEditUrl(carePlan.id, null, getCarePlanOrganization(carePlan))"
                  class="btn">
                 {{ t('Edit')}}
               </a>
@@ -42,7 +42,7 @@
     </table>
   </div>
 </template>
-<script>
+<script setup>
 import { computed, onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import useCarePlanRepository from '../../functions/carePlanRepository';
@@ -52,87 +52,83 @@ import useUrlHelper from '../../functions/urlHelper';
 import TokenStatusBar from './TokenStatusBar.vue';
 import TokenProgress from './TokenProgress.vue';
 
-export default {
-  props: {
-    showDeleted: {
-      type: Boolean,
-      default: true,
-    },
-    showTokenProgress: {
-      type: Boolean,
-      default: false,
-    },
-    showTokenStatusBar: {
-      type: Boolean,
-      default: false,
-    },
+const props = defineProps({
+  showDeleted: {
+    type: Boolean,
+    default: true,
   },
-  components: {
-    TokenProgress,
-    TokenStatusBar,
+  showTokenProgress: {
+    type: Boolean,
+    default: false,
   },
-  setup(props) {
-    const carePlans = ref(null);
-    const questionnaireTasks = ref(null);
-    const { t } = useI18n();
-    const { formatJsonDate } = useDateFunctions();
-
-    const { getCarePlanEditUrl, getCarePlanShowUrl } = useUrlHelper();
-
-    const { getAllCarePlans } = useCarePlanRepository();
-    const { getAllTokens, groupByCarePlans } = useTokenRepository();
-
-    const getCarePlans = (async () => {
-      carePlans.value = await getAllCarePlans();
-      questionnaireTasks.value = await getAllTokens();
-    });
-
-    const filteredCarePlans = computed(() => {
-      if (props.showDeleted === false && carePlans.value !== null) {
-        return carePlans.value.filter((carePlan) => carePlan.status !== 'revoked');
-      }
-      return carePlans.value;
-    });
-
-    const getCarePlanCreator = ((carePlan) => {
-      let creatorName = null;
-      if ('contributor' in carePlan && Array.isArray(carePlan.contributor)) {
-        carePlan.contributor.forEach((contributor) => {
-          if ('type' in contributor && contributor.type === 'staff' && 'display' in contributor) {
-            creatorName = contributor.display;
-          }
-        });
-      }
-      return creatorName;
-    });
-
-    const carePlanTasks = computed(() => {
-      if (questionnaireTasks.value !== null) {
-        return groupByCarePlans(questionnaireTasks.value);
-      }
-      return {};
-    });
-
-    const isDeleted = ((carePlan) => {
-      return carePlan.status === 'revoked' || carePlan.status === 'unknown';
-    });
-
-    onMounted(() => {
-      getCarePlans();
-      getAllTokens();
-    });
-
-    return {
-      carePlans,
-      carePlanTasks,
-      filteredCarePlans,
-      formatJsonDate,
-      getCarePlanCreator,
-      getCarePlanEditUrl,
-      getCarePlanShowUrl,
-      isDeleted,
-      t,
-    };
+  showTokenStatusBar: {
+    type: Boolean,
+    default: false,
   },
-};
+});
+
+const carePlans = ref(null);
+const questionnaireTasks = ref(null);
+const { t } = useI18n();
+const { formatJsonDate } = useDateFunctions();
+
+const { getCarePlanEditUrl, getCarePlanShowUrl } = useUrlHelper();
+
+const { getAllCarePlans } = useCarePlanRepository();
+const { getAllTokens, groupByCarePlans } = useTokenRepository();
+
+const getCarePlans = (async () => {
+  carePlans.value = await getAllCarePlans();
+  questionnaireTasks.value = await getAllTokens();
+});
+
+const filteredCarePlans = computed(() => {
+  if (props.showDeleted === false && carePlans.value !== null) {
+    return carePlans.value.filter((carePlan) => carePlan.status !== 'revoked');
+  }
+  return carePlans.value;
+});
+
+const getCarePlanCreator = ((carePlan) => {
+  let creatorName = null;
+  if ('contributor' in carePlan && Array.isArray(carePlan.contributor)) {
+    carePlan.contributor.forEach((contributor) => {
+      if ('type' in contributor && contributor.type === 'staff' && 'display' in contributor) {
+        creatorName = contributor.display;
+      }
+    });
+  }
+  return creatorName;
+});
+
+const getCarePlanOrganization = ((carePlan) => {
+  if ('contributor' in carePlan && Array.isArray(carePlan.contributor)) {
+    let organizationId = null;
+    carePlan.contributor.forEach((contributor) => {
+      if ('type' in contributor && contributor.type === 'Organization' && 'id' in contributor) {
+        organizationId = contributor.id;
+      }
+    });
+    return organizationId;
+  }
+
+  return null;
+});
+
+const carePlanTasks = computed(() => {
+  if (questionnaireTasks.value !== null) {
+    return groupByCarePlans(questionnaireTasks.value);
+  }
+  return {};
+});
+
+const isDeleted = ((carePlan) => {
+  return carePlan.status === 'revoked' || carePlan.status === 'unknown';
+});
+
+onMounted(() => {
+  getCarePlans();
+  getAllTokens();
+});
+
 </script>
